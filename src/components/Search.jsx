72194c1ff14d024bfdb4ac658c478d29a76dda5a
Ewicks <ethanowicks9@gmail.com>
2023-03-09 23:11:09 +0000

    Add search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+import { useState } from 'react'
+import { FaSearch } from "react-icons/fa"
+
+export default function Search() {
+  return (
+    <FormStyle>
+        <div>
+            <FaSearch></FaSearch>
+            <input type='text' />
+        </div>
+    </FormStyle>
+  )
+}
+
+const FormStyle = styled.div`
+    margin: 0rem 20rem;
+    div {
+        position: relative;
+        width: 100%;
+    }
+
+    input {
+        border: none;
+        background: linear-gradient(35deg, #494949, #313131);
+        font-size: 1.5 rem;
+        color: white;
+        padding: 1rem 3rem;
+        border: none;
+        border-radius: 1rem;
+        outline: none;
+        width: 100%;
+    }
+    svg {
+        position: absolute;
+        top: 50%;
+        left: 0%;
+        transform: translate(100%, -50%);
+        color: white;
+    }
+`
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from './Home'
+import Cuisine from './Cuisine'
+import {Route, Routes } from 'react-router-dom'
+
 
 export default function Pages() {
   return (
-    <div>
-        <Home />
-    </div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+    </Routes>
   )
 }
